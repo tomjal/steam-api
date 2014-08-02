6da4384aec58bb87a3af3aba33e74ebac051d3df
go-ive <ivan.antesklobucar@gmail.com>
2014-08-03 01:22:01 +0200

    added country code support to specify returned currency

--- src/main/java/com/github/goive/steamapi/client/SteamApiClientImpl.java
+++ src/main/java/com/github/goive/steamapi/client/SteamApiClientImpl.java
@@ -9,22 +9,24 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.github.goive.steamapi.enums.CountryCode;
 import com.github.goive.steamapi.exceptions.SteamApiException;
 
 /**
  * The client that connects to the Steam API to retrieve the data for the given appId.
  * 
  * @author Ivan Antes-Klobucar
- * @version 1.1.1
+ * @version 2.1
  */
 public class SteamApiClientImpl implements SteamApiClient {
 
     private static final int MAX_APPIDS = 1000;
     private String apiUrl = "http://store.steampowered.com/api/appdetails?appids=";
     private ObjectMapper mapper = new ObjectMapper();
+    private CountryCode countryCode;
 
     public SteamApiClientImpl() {
-
+        countryCode = CountryCode.AT;
     }
 
     /**
@@ -33,7 +35,17 @@ public class SteamApiClientImpl implements SteamApiClient {
      * @param customApiUrl Overrides the standard API url
      */
     public SteamApiClientImpl(String customApiUrl) {
+        this();
         this.apiUrl = customApiUrl;
+    }
+
+    /**
+     * Creates a new instance.
+     * 
+     * @param countryCode Overrides the default country code to return the correct currency.
+     */
+    public SteamApiClientImpl(CountryCode countryCode) {
+        this.countryCode = countryCode;
     }
 
     @SuppressWarnings("unchecked")
@@ -41,7 +53,7 @@ public class SteamApiClientImpl implements SteamApiClient {
         Map<Object, Object> resultMap = new HashMap<Object, Object>();
 
         try {
-            URL src = new URL(apiUrl + appIds);
+            URL src = new URL(apiUrl + appIds + "&cc=" + countryCode.name());
             resultMap = mapper.readValue(src, Map.class);
         } catch (IOException e) {
             throw new SteamApiException(e);
@@ -77,6 +89,14 @@ public class SteamApiClientImpl implements SteamApiClient {
         resultMap = fetchResultMap(appIdCsvList);
 
         return resultMap;
+    }
+
+    public CountryCode getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(CountryCode countryCode) {
+        this.countryCode = countryCode;
     }
 
 }
